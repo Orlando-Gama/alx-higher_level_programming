15ae5334600f9dd51ced1d0a0aa41e0c4803a37e
Orlalndo-Gama <orlando03gama@gmail.com>
2023-08-29 12:43:32 -0700

    102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    def area(self):
+        return self.__size ** 2
+    
+    def __eq__(self, other):
+        return self.area() == other.area()
+    
+    def __ne__(self, other):
+        return self.area() != other.area()
+    
+    def __lt__(self, other):
+        return self.area() < other.area()
+    
+    def __le__(self, other):
+        return self.area() <= other.area()
+    
+    def __gt__(self, other):
+        return self.area() > other.area()
+    
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+# Test the Square class
+s_5 = Square(5)
+s_6 = Square(6)
+
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
+
