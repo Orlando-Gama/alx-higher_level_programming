831bea98d9d4c73c2d61a4be85d38b70d450cf44
Orlando-Gama <orlando03gama@gmail.com>
2024-01-03 08:07:04 -0800

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,28 @@
 #!/usr/bin/node
 
-const req = require('request');
+const axios = require('axios');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
-    });
-  }
-});
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+axios.get(url)
+  .then(response => {
+    const data = response.data;
+    const characters = data.characters;
+
+    // Use Promise.all to handle multiple asynchronous requests
+    const characterPromises = characters.map(characterUrl => axios.get(characterUrl));
+
+    Promise.all(characterPromises)
+      .then(responses => {
+        for (const response of responses) {
+          const characterData = response.data;
+          console.log(characterData.name);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching character data:', error);
+      });
+  })
+  .catch(error => {
+    console.error('Error fetching film data:', error);
+  });
